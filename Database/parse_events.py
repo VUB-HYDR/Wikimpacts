36120071af716436f6906186f1507c492510d928
Shorouq <shorouq.zahra@ri.se>
2024-03-25 11:45:47 +0100

    🧐 Fix postprocessing after change in json schema

--- Database/parse_events.py
+++ Database/parse_events.py
@@ -1,4 +1,5 @@
 import json
+import re
 
 import pandas as pd
 import shortuuid
@@ -9,8 +10,17 @@ pd.set_option("display.max_colwidth", 30)
 pd.set_option("display.max_rows", None)
 
 
-def random_short_uuid(x, length: int = 7):
+def random_short_uuid(length: int = 7):
+    """Generates a short alpha-numerical UID"""
     return shortuuid.ShortUUID().random(length=length)
+
+
+def replace_nulls(df: pd.DataFrame) -> pd.DataFrame:
+    """Normalizes all variations of NULL, NaN, nan, null either as data types or as strings"""
+    any_NULL = re.compile(r"^(null)$|^(nan)$", re.IGNORECASE | re.MULTILINE)
+    df = df.replace(any_NULL, None)
+    df = df.astype(object).where(pd.notnull(df), None)
+    return df
 
 
 def parse_basic_info(basic_series: pd.Series) -> pd.DataFrame:
@@ -25,6 +35,11 @@ def parse_basic_info(basic_series: pd.Series) -> pd.DataFrame:
         output.append(entry)
     return pd.DataFrame(output)
 
+
+def parse_specific_info():
+    pass
+
+
 def normalize_date(row) -> tuple[int, int, int]:  # tuple[datetime, tuple]:
     """
     See https://github.com/scrapinghub/dateparser/issues/700
@@ -33,8 +48,13 @@ def normalize_date(row) -> tuple[int, int, int]:  # tuple[datetime, tuple]:
     Returns a tuple: (day, month, year) with None for missing values
     """
     if row is not None:
-        # TODO: we assume we need at least a month and a year, talk to Ni about this
-        ddp = DateDataParser(settings={"REQUIRE_PARTS": ["year"], "NORMALIZE": True, "PREFER_DATES_FROM": "past"})
+        ddp = DateDataParser(
+            settings={
+                "REQUIRE_PARTS": ["year"],
+                "NORMALIZE": True,
+                "PREFER_DATES_FROM": "past",
+            }
+        )
 
         year, month, day = "%Y", "%m", "%d"
 
@@ -49,23 +69,22 @@ def normalize_date(row) -> tuple[int, int, int]:  # tuple[datetime, tuple]:
                 return (None, None, date.date_obj.strftime(year))
 
             elif date.period == "month":
-                return (None, date.date_obj.strftime(month), date.date_obj.strftime(year))
+                return (
+                    None,
+                    date.date_obj.strftime(month),
+                    date.date_obj.strftime(year),
+                )
 
             elif date.period == "day":
-                return (date.date_obj.strftime(day), date.date_obj.strftime(month), date.date_obj.strftime(year))
+                return (
+                    date.date_obj.strftime(day),
+                    date.date_obj.strftime(month),
+                    date.date_obj.strftime(year),
+                )
 
         except BaseException as err:
             print(f"Date parsing error in {row} with date\n{err}\n")
             return (None, None, None)
-
-
-def parse_json(x):
-    try:
-        return json.loads(x)
-    except json.JSONDecodeError as err:
-        print("Skipping parsing for:\n", x)
-        print(err)
-        return {"Could not parse": str(x)}
 
 
 def flatten_dicts(json_dict_list: list[str]) -> dict:
@@ -87,62 +106,57 @@ def flatten_dicts(json_dict_list: list[str]) -> dict:
     return output
 
 
+def unpack_col(df, columns: list = []):
+    for c in columns:
+        df = pd.concat([pd.json_normalize(df[c]), df], axis=1)
+        df.drop(columns=[c], inplace=True)
+    return df
+
+
 if __name__ == "__main__":
-    # data_path = "Database/raw/response_wiki_GPT4_20240315_13events.json"
-    data_path = "Database/raw/response_wiki_GPT4_20240317_No1_79.json"
+    # load raw file
+    filename = "response_wiki_GPT4_20240320_test_hurricane_agnes.json"
+    data_path = f"Database/raw/{filename}"
     df = pd.read_json(data_path)
 
+    # unpack total_summary columns
+    total_summary_cols = [col for col in df if col.startswith("Total_")]
+    df = unpack_col(df, columns=total_summary_cols)
+
     # add short uids for each event
-    df["Event_ID"] = df["Event_Name"].apply(random_short_uuid)
-
-    # parse basic info and add to flat table
-    basic = parse_basic_info(df["basic"])
-    data = pd.concat([df, basic], axis=1)
-    data.drop(["basic"], axis=1, inplace=True)
-
-    # remove "NULL" and "NAN" strings
-    data.replace("NAN", None, inplace=True)
-    data.replace("NULL", None, inplace=True)
+    df["Event_ID"] = random_short_uuid(length=7)
 
     # normalize dates
-    Start_Dates = data.Start_Date.apply(normalize_date)
-    End_Dates = data.End_Date.apply(normalize_date)
+    Start_Dates = df.Start_Date.apply(normalize_date)
+    End_Dates = df.End_Date.apply(normalize_date)
 
     Start_Date_Cols = pd.DataFrame(
-        Start_Dates.to_list(), columns=["Start_Date_Day", "Start_Date_Month", "Start_Date_Year"]
+        Start_Dates.to_list(),
+        columns=["Start_Date_Day", "Start_Date_Month", "Start_Date_Year"],
     )
     End_Date_Cols = pd.DataFrame(End_Dates.to_list(), columns=["End_Date_Day", "End_Date_Month", "End_Date_Year"])
 
-    data = pd.concat([data, Start_Date_Cols, End_Date_Cols], axis=1)
+    df = pd.concat([df, Start_Date_Cols, End_Date_Cols], axis=1)
 
-    impact_data = data[["Event_ID", "impact"]]
-    impact_data = impact_data.explode("impact")
-
-    impact_data.impact = impact_data.impact.apply(parse_json)
-
-    total_rows = []
-
-    for i in range(impact_data.Event_ID.nunique()):
-        total_rows.append(flatten_dicts(impact_data.loc[i].impact.to_list()))
-
-    # print(total_rows)
-    total_rows = pd.DataFrame(total_rows)
-    flat_data = pd.concat([data, total_rows], axis=1)
-
-    flat_data.Total_Insured_Damage_Inflation_Adjusted.replace({"No": False, "Yes": True}, inplace=True)
-    flat_data.Total_Damage_Inflation_Adjusted.replace({"No": False, "Yes": True}, inplace=True)
-
-    flat_data.drop("impact", inplace=True, axis=1)
-
-    # print(flat_data.columns)
-    # print(flat_data.shape)
-    # print(flat_data)
-
-    # cast annotation fields as str
-    flat_data = flat_data.astype(str)
-
-    # print(flat_data.dtypes)
-    flat_data.to_parquet(
-        "Database/output/flat_basic_and_impact_79_dmy.parquet",
+    _yes, _no = re.compile(r"^(yes)$|^(y)$|^(true)$", re.IGNORECASE | re.MULTILINE), re.compile(
+        r"^(no)$|^(n)$|^(false)$", re.IGNORECASE | re.MULTILINE
     )
-    flat_data = pd.read_parquet("Database/output/flat_basic_and_impact_79_dmy.parquet")
+
+    df.Total_Insured_Damage_Inflation_Adjusted = df.Total_Insured_Damage_Inflation_Adjusted.replace(
+        {_no: False, _yes: True}, regex=True
+    )
+    df.Total_Damage_Inflation_Adjusted = df.Total_Damage_Inflation_Adjusted.replace(
+        {_no: False, _yes: True}, regex=True
+    )
+
+    # clean out NaNs and Nulls
+    df = replace_nulls(df)
+
+    # save data
+    # df = df.astype(str)
+    parquet_filename = f"{filename.split('.json')[0]}.parquet"
+    df.to_parquet(parquet_filename)
+
+    # check that the parquet was stored correctly
+    flat_data = pd.read_parquet(parquet_filename)
+    print(flat_data)
