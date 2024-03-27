f8426c887a277076d0e8de97f6254d125cebbd2e
Shorouq <shorouq.zahra@ri.se>
2024-03-27 11:35:45 +0100

    🗃️ Json-normalize specific impact data

--- Database/parse_events.py
+++ Database/parse_events.py
@@ -116,19 +116,20 @@ def unpack_col(df, columns: list = []):
 if __name__ == "__main__":
     # load raw file
     filename = "response_wiki_GPT4_20240320_test_hurricane_agnes.json"
-    data_path = f"Database/raw/{filename}"
-    df = pd.read_json(data_path)
+    raw_path = "Database/raw"
+    output_path = "Database/output"
+    df = pd.read_json(f"{raw_path}/{filename}")
 
     # unpack total_summary columns
     total_summary_cols = [col for col in df if col.startswith("Total_")]
-    df = unpack_col(df, columns=total_summary_cols)
+    total_summary = unpack_col(df, columns=total_summary_cols)
 
     # add short uids for each event
-    df["Event_ID"] = random_short_uuid(length=7)
+    total_summary["Event_ID"] = random_short_uuid(length=7)
 
     # normalize dates
-    Start_Dates = df.Start_Date.apply(normalize_date)
-    End_Dates = df.End_Date.apply(normalize_date)
+    Start_Dates = total_summary.Start_Date.apply(normalize_date)
+    End_Dates = total_summary.End_Date.apply(normalize_date)
 
     Start_Date_Cols = pd.DataFrame(
         Start_Dates.to_list(),
@@ -136,27 +137,42 @@ if __name__ == "__main__":
     )
     End_Date_Cols = pd.DataFrame(End_Dates.to_list(), columns=["End_Date_Day", "End_Date_Month", "End_Date_Year"])
 
-    df = pd.concat([df, Start_Date_Cols, End_Date_Cols], axis=1)
+    total_summary = pd.concat([total_summary, Start_Date_Cols, End_Date_Cols], axis=1)
 
     _yes, _no = re.compile(r"^(yes)$|^(y)$|^(true)$", re.IGNORECASE | re.MULTILINE), re.compile(
         r"^(no)$|^(n)$|^(false)$", re.IGNORECASE | re.MULTILINE
     )
 
-    df.Total_Insured_Damage_Inflation_Adjusted = df.Total_Insured_Damage_Inflation_Adjusted.replace(
+    total_summary.Total_Insured_Damage_Inflation_Adjusted = total_summary.Total_Insured_Damage_Inflation_Adjusted.replace(
         {_no: False, _yes: True}, regex=True
     )
-    df.Total_Damage_Inflation_Adjusted = df.Total_Damage_Inflation_Adjusted.replace(
+    total_summary.Total_Damage_Inflation_Adjusted = total_summary.Total_Damage_Inflation_Adjusted.replace(
         {_no: False, _yes: True}, regex=True
     )
 
     # clean out NaNs and Nulls
-    df = replace_nulls(df)
+    total_summary = replace_nulls(total_summary)
 
     # save data
-    # df = df.astype(str)
-    parquet_filename = f"output/{filename.split('.json')[0]}.parquet"
-    df.to_parquet(parquet_filename)
+    # total_summary = total_summary.astype(str)
+    total_summary_parquet_filename = f"{output_path}/{filename.split('.json')[0]}.parquet"
+    total_summary.to_parquet(total_summary_parquet_filename)
+
+    # get specific impact summaries
+    specific_summary_cols = [col for col in total_summary if col.startswith("Specific_")]
+
+    specifc_summary_dfs = []
+
+    for col in specific_summary_cols:
+        assert df[col].shape == (1,), "Bad shape, not a list; check the data"
+        normalized_df = pd.json_normalize(df[col][0])
+        specifc_summary_dfs.append(normalized_df)
+        
+    for col_name, data in zip(specific_summary_cols, specifc_summary_dfs):
+        data.to_parquet(f"{output_path}/{col_name}.parquet")
 
     # check that the parquet was stored correctly
-    flat_data = pd.read_parquet(parquet_filename)
-    print(flat_data)
+    # flat_data = pd.read_parquet(total_summary_parquet_filename)
+    # print(flat_data)
+    # specific_data = pd.read_parquet(f"{output_path}/{specific_summary_cols[0]}.parquet")
+    # print(specific_data)
