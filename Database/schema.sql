7a4671d1e20fd53a72ed47bab236239d849fb195
Shorouq <shorouq.zahra@ri.se>
2024-03-28 11:33:32 +0100

    🗃️ Update schema with start+end dates for subevents

--- Database/schema.sql
+++ Database/schema.sql
@@ -6,18 +6,18 @@ CREATE TABLE Events (
     Main_Event TEXT NOT NULL, /* COMMENT 'Categorical', */
     Perils OBJECT, /* COMMENT 'Array', */
     Location OBJECT, /* COMMENT 'Array', */
-    Single_Date DATE,
+    /*  Single_Date DATE, */
     /* Single_Date_Day INTEGER CHECK (Single_Date_Day <= 31), */
     /* Single_Date_Month INTEGER CHECK (Single_Date_Month <= 12), */
     /* Single_Date_Year INTEGER CHECK (length(Single_Date_Year) = 4), */
     Start_Date DATE,
-    /* Start_Date_Day INTEGER CHECK (Start_Date_Day <= 31), */
-    /* Start_Date_Month INTEGER CHECK (Start_Date_Month <= 12), */
-    /* Start_Date_Year INTEGER CHECK (length(Start_Date_Year) = 4), */
+    Start_Date_Day INTEGER CHECK (Start_Date_Day <= 31),
+    Start_Date_Month INTEGER CHECK (Start_Date_Month <= 12),
+    Start_Date_Year INTEGER CHECK (length(Start_Date_Year) = 4),
     End_Date DATE,
-    /* End_Date_Day INTEGER CHECK (End_Date_Day <= 31), */
-    /* End_Date_Month INTEGER CHECK (End_Date_Month <= 12), */
-    /* End_Date_Year INTEGER CHECK (length(End_Date_Year) = 4), */
+    End_Date_Day INTEGER CHECK (End_Date_Day <= 31),
+    End_Date_Month INTEGER CHECK (End_Date_Month <= 12),
+    End_Date_Year INTEGER CHECK (length(End_Date_Year) = 4),
     Total_Deaths INTEGER,
     Total_Injuries INTEGER,
     Total_Displaced INTEGER,
@@ -41,10 +41,14 @@ CREATE TABLE Specific_Instance_Per_Country_Death (
     Country TEXT NOT NULL, /* COMMENT 'Country Code', */
     Location_Death TEXT,
     Location_GCS OBJECT, /* COMMENT 'GCS', */
-    Time_Death DATE,
-    Time_Death_Day INTEGER CHECK (Time_Death_Day <= 31),
-    Time_Death_Month INTEGER CHECK (Time_Death_Month <= 12),
-    Time_Death_Year INTEGER CHECK (length(Time_Death_Year) = 4),
+    Start_Date_Death DATE,
+    Start_Date_Death_Day INTEGER CHECK (Start_Date_Death_Day <= 31),
+    Start_Date_Death_Month INTEGER CHECK (Start_Date_Death_Month <= 12),
+    Start_Date_Death_Year INTEGER CHECK (length(Start_Date_Death_Year) = 4),
+    End_Date_Death DATE,
+    End_Date_Death_Day INTEGER CHECK (End_Date_Death_Day <= 31),
+    End_Date_Death_Month INTEGER CHECK (End_Date_Death_Month <= 12),
+    End_Date_Death_Year INTEGER CHECK (length(End_Date_Death_Year) = 4),
     Num_Death INTEGER,
     FOREIGN KEY(Event_ID) REFERENCES Events(Event_ID)
 );
@@ -54,10 +58,14 @@ CREATE TABLE Specific_Instance_Per_Country_Injuries (
     Country TEXT NOT NULL, /* COMMENT 'Country Code', */
     Location_Injuries TEXT,
     Location_GCS OBJECT, /* COMMENT 'GCS', */
-    Time_Injuries DATE,
-    Time_Injuries_Day INTEGER CHECK (Time_Injuries_Day <= 31),
-    Time_Injuries_Month INTEGER CHECK (Time_Injuries_Month <= 12),
-    Time_Injuries_Year INTEGER CHECK (length(Time_Injuries_Year) = 4),
+    Start_Date_Injuries DATE,
+    Start_Date_Injuries_Day INTEGER CHECK (Start_Date_Injuries_Day <= 31),
+    Start_Date_Injuries_Month INTEGER CHECK (Start_Date_Injuries_Month <= 12),
+    Start_Date_Injuries_Year INTEGER CHECK (length(Start_Date_Injuries_Year) = 4),
+    End_Date_Injuries DATE,
+    End_Date_Injuries_Day INTEGER CHECK (End_Date_Injuries_Day <= 31),
+    End_Date_Injuries_Month INTEGER CHECK (End_Date_Injuries_Month <= 12),
+    End_Date_Injuries_Year INTEGER CHECK (length(End_Date_Injuries_Year) = 4),
     Num_Injuries INTEGER,
     FOREIGN KEY(Event_ID) REFERENCES Events(Event_ID)
 );
@@ -67,10 +75,14 @@ CREATE TABLE Specific_Instance_Per_Country_Displacement (
     Country TEXT NOT NULL, /* COMMENT 'Country Code', */
     Location_Displace TEXT,
     Location_GCS OBJECT, /* COMMENT 'GCS', */
-    Time_Displaced DATE,
-    Time_Displaced_Day INTEGER CHECK (Time_Displaced_Day <= 31),
-    Time_Displaced_Month INTEGER CHECK (Time_Displaced_Month <= 12),
-    Time_Displaced_Year INTEGER CHECK (length(Time_Displaced_Year) = 4),
+    Start_Date_Displacement DATE,
+    Start_Date_Displacement_Day INTEGER CHECK (Start_Date_Displacementd_Day <= 31),
+    Start_Date_Displacement_Month INTEGER CHECK (Start_Date_Displacementd_Month <= 12),
+    Start_Date_Displacement_Year INTEGER CHECK (length(Start_Date_Displacementd_Year) = 4),
+    End_Date_Displacement DATE,
+    End_Date_Displacement_Day INTEGER CHECK (End_Date_Displacementd_Day <= 31),
+    End_Date_Displacement_Month INTEGER CHECK (End_Date_Displacementd_Month <= 12),
+    End_Date_Displacement_Year INTEGER CHECK (length(End_Date_Displacementd_Year) = 4),
     Num_Displaced INTEGER,
     FOREIGN KEY(Event_ID) REFERENCES Events(Event_ID)
 );
@@ -80,10 +92,14 @@ CREATE TABLE Specific_Instance_Per_Country_Homelessness (
     Country TEXT NOT NULL, /* COMMENT 'Country Code', */
     Location_Homeless TEXT,
     Location_GCS OBJECT, /* COMMENT 'GCS', */
-    Time_Homeless DATE,
-    Time_Homeless_Day INTEGER CHECK (Time_Homeless_Day <= 31),
-    Time_Homeless_Month INTEGER CHECK (Time_Homeless_Month <= 12),
-    Time_Homeless_Year INTEGER CHECK (length(Time_Homeless_Year) = 4),
+    Start_Date_Homelessness DATE,
+    Start_Date_Homelessness_Day INTEGER CHECK (Start_Date_Homelessness_Day <= 31),
+    Start_Date_Homelessness_Month INTEGER CHECK (Start_Date_Homelessness_Month <= 12),
+    Start_Date_Homelessness_Year INTEGER CHECK (length(Start_Date_Homelessness_Year) = 4),
+    End_Date_Homelessness DATE,
+    End_Date_Homelessness_Day INTEGER CHECK (End_Date_Homelessness_Day <= 31),
+    End_Date_Homelessness_Month INTEGER CHECK (End_Date_Homelessness_Month <= 12),
+    End_Date_Homelessness_Year INTEGER CHECK (length(End_Date_Homelessness_Year) = 4),
     Num_Homeless INTEGER,
     FOREIGN KEY(Event_ID) REFERENCES Events(Event_ID)
 );
@@ -109,9 +125,7 @@ CREATE TABLE Specific_Instance_Per_Country_Economic_Damage (
     Location_GCS OBJECT, /* COMMENT 'GCS', */
     Damage REAL,
     Damage_Units TEXT, /* COMMENT 'currency', */
-    Damage_Inflation_Adjusted INTEGER, /* COMMENT 'Boolean', */
-    Damage_Inflation_Adjusted_Year INTEGER CHECK (length(Damage_Inflation_Adjusted_Year) = 4),
-    FOREIGN KEY(Event_ID) REFERENCES Events(Event_ID)
+    Damage_Inflation_Adjusted INTEGER,/* COMMENT 'Boolean', */Damage_Inflation_Adjusted_Year INTEGER CHECK (length(Damage_Inflation_Adjusted_Year) = 4),FOREIGN KEY(Event_ID) REFERENCES Events(Event_ID)
 );
 
 CREATE TABLE Specific_Instance_Per_Country_Building_Damage (
@@ -119,10 +133,14 @@ CREATE TABLE Specific_Instance_Per_Country_Building_Damage (
     Country TEXT NOT NULL, /* COMMENT 'Country Code', */
     Location_Building TEXT,
     Location_GCS OBJECT, /* COMMENT 'GCS', */
-    Time_Building DATE,
-    Time_Building_Day INTEGER CHECK (Time_Building_Day <= 31),
-    Time_Building_Month INTEGER CHECK (Time_Building_Month <= 12),
-    Time_Building_Year INTEGER CHECK (length(Time_Building_Year) = 4),
+    Start_Date_Building DATE,
+    Start_Date_Building_Day INTEGER CHECK (Start_Date_Building_Day <= 31),
+    Start_Date_Building_Month INTEGER CHECK (Start_Date_Building_Month <= 12),
+    Start_Date_Building_Year INTEGER CHECK (length(Start_Date_Building_Year) = 4),
+    End_Date_Building DATE,
+    End_Date_Building_Day INTEGER CHECK (End_Date_Building_Day <= 31),
+    End_Date_Building_Month INTEGER CHECK (End_Date_Building_Month <= 12),
+    End_Date_Building_Year INTEGER CHECK (length(End_Date_Building_Year) = 4),
     Buildings_Damaged REAL,
     FOREIGN KEY(Event_ID) REFERENCES Events(Event_ID)
 );
