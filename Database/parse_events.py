4d780f56fb25e71a22623bee7b99c32b4fb5a3c6
Shorouq <shorouq.zahra@ri.se>
2024-03-28 11:09:03 +0100

    🧐 Parse subevents

--- Database/parse_events.py
+++ Database/parse_events.py
@@ -1,3 +1,4 @@
+import ast
 import re
 
 import pandas as pd
@@ -79,76 +80,89 @@ def unpack_col(df: pd.DataFrame, columns: list = []) -> pd.DataFrame:
 
 if __name__ == "__main__":
     # load raw file
-    filename = "response_wiki_GPT4_20240320_test_hurricane_agnes.json"
+    filename = "response_wiki_GPT4_20240327_eventNo_1_8_all_category.json"
     raw_path = "Database/raw"
     output_path = "Database/output"
     df = pd.read_json(f"{raw_path}/{filename}")
 
-    # unpack total_summary columns
-    total_summary_cols = [col for col in df if col.startswith("Total_")]
-    total_summary = unpack_col(df, columns=total_summary_cols)
-
     # add short uids for each event
-    total_summary["Event_ID"] = random_short_uuid(length=7)
+    df["Event_ID"] = [random_short_uuid() for _ in df.index]
+
+    # unpack Total_Summary_* columns
+    total_summary_cols = [col for col in df if col.startswith("Total_")]
+    events = unpack_col(df, columns=total_summary_cols)
+
+    del df
+    # add short uids for each event
+    # total_summary["Event_ID"] = random_short_uuid(length=7)
 
     # normalize dates
-    Start_Dates = total_summary.Start_Date.apply(normalize_date)
-    End_Dates = total_summary.End_Date.apply(normalize_date)
+    start_dates = events.Start_Date.apply(normalize_date)
+    end_dates = events.End_Date.apply(normalize_date)
 
-    Start_Date_Cols = pd.DataFrame(
-        Start_Dates.to_list(),
+    start_date_cols = pd.DataFrame(
+        start_dates.to_list(),
         columns=["Start_Date_Day", "Start_Date_Month", "Start_Date_Year"],
     )
-    End_Date_Cols = pd.DataFrame(End_Dates.to_list(), columns=["End_Date_Day", "End_Date_Month", "End_Date_Year"])
+    end_date_cols = pd.DataFrame(end_dates.to_list(), columns=["End_Date_Day", "End_Date_Month", "End_Date_Year"])
 
-    total_summary = pd.concat([total_summary, Start_Date_Cols, End_Date_Cols], axis=1)
+    events = pd.concat([events, start_date_cols, end_date_cols], axis=1)
+    del start_dates, end_dates, start_date_cols, end_date_cols
 
     # normalize binary categories into booleans
     _yes, _no = re.compile(r"^(yes)$|^(y)$|^(true)$", re.IGNORECASE | re.MULTILINE), re.compile(
         r"^(no)$|^(n)$|^(false)$", re.IGNORECASE | re.MULTILINE
     )
 
-    total_summary.Total_Insured_Damage_Inflation_Adjusted = (
-        total_summary.Total_Insured_Damage_Inflation_Adjusted.replace({_no: False, _yes: True}, regex=True)
+    events.Total_Insured_Damage_Inflation_Adjusted = events.Total_Insured_Damage_Inflation_Adjusted.replace(
+        {_no: False, _yes: True}, regex=True
     )
-    total_summary.Total_Damage_Inflation_Adjusted = total_summary.Total_Damage_Inflation_Adjusted.replace(
+    events.Total_Damage_Inflation_Adjusted = events.Total_Damage_Inflation_Adjusted.replace(
         {_no: False, _yes: True}, regex=True
     )
 
+    # normalize Perils into a list
+    events.Perils = events.Perils.apply(lambda x: x.split("|"))
+
     # clean out NaNs and Nulls
-    total_summary = replace_nulls(total_summary)
+    events = replace_nulls(events)
 
-    total_summary_parquet_filename = f"{output_path}/{filename.split('.json')[0]}.parquet"
-    total_summary.to_parquet(total_summary_parquet_filename)
+    # convert to string bytes (for the Perils_Assessment_With_Annotation column)
+    events = events.astype(str)
 
-    # get specific impact summaries
-    specific_summary_cols = [col for col in total_summary if col.startswith("Specific_")]
+    # store as parquet
+    events_parquet_filename = f"{output_path}/{filename.split('.json')[0]}.parquet"
+    events.to_parquet(events_parquet_filename)
+
+    specific_summary_cols = [col for col in events if col.startswith("Specific_")]
     specifc_summary_dfs = {}
 
-    # normalize the json columns to extract subevents tables
     for col in specific_summary_cols:
-        assert df[col].shape == (1,), "Bad shape, not a list; check the data"
-        normalized_df = pd.json_normalize(df[col][0])
-        specifc_summary_dfs[col] = normalized_df
+        # evaluate string bytes to dict
+        events[col] = events[col].astype(str)
+        events[col] = events[col].apply(ast.literal_eval)
 
-    # normalize dates (only for subevent data tables that have a Time_* field)
-    specific_summary_with_dates = (
-        ("Specific_Instance_Per_Country_Death", "Time_Death"),
-        ("Specific_Instance_Per_Country_Injuries", "Time_Injuries"),
-        ("Specific_Instance_Per_Country_Displacement", "Time_Displace"),
-        ("Specific_Instance_Per_Country_Homelessness", "Time_Homeless"),
-        ("Specific_Instance_Per_Country_Building_Damage", "Time_Building"),
-    )
+        # unpack subevents
+        sub_event = events[["Event_ID", col]].explode(col)
 
-    for table_name, col_name in specific_summary_with_dates:
-        date = specifc_summary_dfs[table_name][col_name].apply(normalize_date)
-        date_col = pd.DataFrame(
-            date.to_list(),
-            columns=[f"{col_name}_Day", f"{col_name}_Month", f"{col_name}_Year"],
+        # drop any events that have no subevents (aka [] exploded into NaN)
+        sub_event.dropna(inplace=True)
+        sub_event = pd.concat([sub_event.Event_ID, sub_event[col].apply(pd.Series)], axis=1)
+
+        # clean out nulls
+        sub_event = replace_nulls(sub_event)
+
+        # normalize start and end dates
+        start_dates = sub_event.Start_Date.apply(normalize_date)
+        end_dates = sub_event.End_Date.apply(normalize_date)
+
+        start_date_cols = pd.DataFrame(
+            start_dates.to_list(),
+            columns=["Start_Date_Day", "Start_Date_Month", "Start_Date_Year"],
         )
-        specifc_summary_dfs[table_name] = pd.concat([specifc_summary_dfs[table_name], date_col], axis=1)
+        end_date_cols = pd.DataFrame(end_dates.to_list(), columns=["End_Date_Day", "End_Date_Month", "End_Date_Year"])
 
-    # store subevents in parquet
-    for col_name, data in specifc_summary_dfs.items():
-        data = replace_nulls(data)
-        data.to_parquet(f"{output_path}/{col_name}.parquet")
+        sub_event = pd.concat([sub_event, start_date_cols, end_date_cols], axis=1)
+
+        # store as parquet
+        sub_event.to_parquet(f"{output_path}/{col}.parquet")
