e3889b509dc4208336e867b177d8b8cf6a494221
Shorouq <shorouq.zahra@ri.se>
2024-04-04 11:16:47 +0200

    💡 Add comments to explain the flow (to myself)

--- Database/normalize_nums.py
+++ Database/normalize_nums.py
@@ -21,18 +21,17 @@ except OSError:
 
 def extract_single_number(text: str) -> list[float] | None:
     try:
-        # try extracting the number directly
+        # try extracting the number (in digits) directly
         number = locale.atof(text)
         return [number]
     except:
         try:
-            # try extracting the number from wordsf
+            # try extracting the number from words
             number = text2num(text, lang="en", relaxed=True)
             return [number]
         except BaseException as err:
             try:
                 # try normalizing the numbers to words, then extract the numbers
-                # TODO: generalize to strings that may have multiple numbers in words
                 text = normalize_num_to_words(nlp(text))
                 number = text2num(text, lang="en", relaxed=True)
                 return [number]
@@ -99,6 +98,12 @@ def extract_numbers_from_entities(doc: spacy.tokens.doc.Doc, labels: list[str] =
 
 
 def check_for_inequality_symbol(doc) -> bool:
+    # TODO: the assumption now is that inequality symbols and other math symbols are extracted as NFP by spaCy
+    # But I think the name of the function should change (check for comparatives or inequalities) or something 
+    # and it should grab whatever is before and after the CD block (for robustness, maybe whatever is before the 
+    # first CD and after the last CD, since there are often multiple numbers or a number over a longer span - or potentially
+    # using the spaCy ent span instead)
+    # Then, if any JJR/JJS/NFP exists, it's most likely an estimation 
     tags = [token.tag_ for token in doc]
     return True if "NFP" in tags else False
 
@@ -108,22 +113,31 @@ def extract_numbers(text: str):
     approx = None
     text = text.strip().lower()
     try:
+        # simplest approach, attempt to extract a single number
         numbers = extract_single_number(text)
+        # assume that extracted numbers are not an approximation
         approx = False
         # print("extract_single_number", numbers, approx)
 
     except BaseException:
         # pprint.pprint(doc.to_json(), indent=3)
+        # if extracting a single number fails:
         doc = nlp(text)
         try:
-            # try extraction by spaCy ner
+            # try extraction by spaCy NERs
             numbers = extract_numbers_from_entities(doc)
+
+            # an approx if an inequality symbol exists (>=, ~, etc)
             approx = check_for_inequality_symbol(doc)
             # print("extract_numbers_from_entities", numbers, approx)
         except BaseException:
             try:
-                # try extracting by token
+                # if no NERs were extacted or no NERs were useful, try extracting by token instead
                 numbers = extract_numbers_from_tokens(doc)
+
+                # usually, the chosen spaCy model tends to have a longer span for CD 
+                # if the number is an approximation (such as in 'at least 30 people')
+                # so we'll assume it's an approximation
                 approx = True
                 # print("extract_numbers_from_tokens", numbers, approx)
             except BaseException:
@@ -131,6 +145,9 @@ def extract_numbers(text: str):
                     # if all fails, try by normalizing the numbers to words
                     doc = nlp(normalize_num_to_words(doc))
                     numbers = extract_numbers_from_entities(doc)
+
+                    # an approx if an inequality symbol exists (>=, ~, etc)
+                    # TODO: this could probably be refactored to be done at an earlier stage and determine the approx based on it 
                     approx = check_for_inequality_symbol(doc)
                     # print("EXTRACTED NUMBERS (BaseException)")
                 except BaseException:
@@ -148,7 +165,7 @@ def extract_numbers(text: str):
 
 if __name__ == "__main__":
     examples = [
-        "$692 million",  # ignores the million
+        "$692 million",
         "$ six hundred ninety-two million",
         ">= 200",
         "<55",
@@ -156,42 +173,39 @@ if __name__ == "__main__":
         "~ 4234",
         "no more than 1634",  # considers this as an approx
         "more than 1634 people",
-        "up to 12",  # PROPBLEM WITH IN
+        "up to 12",  # PROPBLEM WITH 'IN'
         "238 people died because of the heat, with 187 injuries.",  # okay parsing but wrong (due to injuries)
-        "between 9 and 66",  # is this approx?
+        "between 9 and 66",  # is this approx? I think not
         "exactly 60",
         "approximately 233",
         "almost 100",
         "no more than 13",
         "160 to say the least",
-        "over 2,000 such visits",  # not approx?  PROPBLEM WITH IN
+        "over 2,000 such visits",  # not approx?  PROPBLEM WITH 'IN'
         "1,100 people were hospitalized",
-        "around 600 excess deaths in the United States, >=229 confirmed in the United States",  # extracts too many numbers
+        "around 600 excess deaths in the United States, >=229 confirmed in the United States",
         "At least 60",
-        "Apple is looking at buying U.K. startup for $1 billion",  # does not parse as 1 billion
+        "Apple is looking at buying U.K. startup for $1 billion",  # parses 1 billion as (1, 1bil)! problematic
         "two billion",
-        "At least 238 people died because of the heat, with 187 injuries.",  # extracts too many numbers
+        "At least 238 people died because of the heat, with 187 injuries.",
         "More than 2,000",
         "More than 27,00,000",
         "337",
         "300 to 400 people",
         "hundreds of people",
-        "A$5 million",  # australian dollars, can't parse
-        "A$ five million",  # australian dollars, can't parse
+        "A$5 million", # parses 5 million as (5, 1mil)! problematic
+        "A$ five million", 
         "$six hundred ninety-two million",
-        "$ six hundred ninety-two million",  # i removed text2num.... this is why this won't work
-        "More than 2 lakhs (200 thousand) people have been affected",  # extracts 4 nums
+        "$ six hundred ninety-two million",
+        "More than 2 lakhs (200 thousand) people have been affected", # problem with "thousand"
         "11,000",
         "one death in Idaho",
         "12 missing",
     ]
 
-    # working_with = ["A$ five million", "A$5 million", "A$ 5 million"]
     print("MIN", "MAX", "APPROX?")
     for i in examples:
-        # find all currency symbols
+        # find all currency symbols and space them out
         i = " ".join(regex.sub(r"\p{Sc}|(~)", " \g<0> ", i).split())
-        # i = " ".join(regex.sub(r"\p{Sm}|\p{Sc}", " \g<0> ", i).split())
-
         print(extract_numbers(i))
         print("--------------------------")
