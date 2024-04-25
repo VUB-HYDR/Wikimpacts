1a73235505a0f1071fd1fff90cd2aab514fe5ae6
Shorouq <shorouq.zahra@ri.se>
2024-04-25 11:16:38 +0200

    ♻️ Refactor GADM id getter for robustness

--- Database/normalize_locs.py
+++ Database/normalize_locs.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import us
+import difflib
+import pprint
+import re
 
 
 class NormalizeLoc:
@@ -22,19 +25,22 @@ class NormalizeLoc:
             # Open Street Map has an issue with "united" countries. "The UK" and "The US" return no results, but "UK" and "US" do.
             query = (
                 {
-                    "country": (
-                        text.replace("the ", "") if text.startswith("the u") else text
+                    "country": 
+                    (
+                        re.sub(r"(the)", "", text).strip() if text.startswith("the u") else text
                     )
                 }
                 if is_country
                 else text
             )
             l = self.geocode(query, exactly_one=True, namedetails=True)
+            # print(l)
+            # pprint.pprint(l.raw)
 
             return (
                 # return the english name only if a country (due to GADM's format)
                 l.raw["namedetails"]["name:en"]
-                if is_country
+                if (is_country or l.raw["addresstype"] == "country")
                 else (
                     # return the international name if present (only for sublocations, due to GADM's format)
                     l.raw["namedetails"]["int_name"]
@@ -49,7 +55,17 @@ class NormalizeLoc:
             )
             return
 
-    def get_gadm_gid(self, area: str = None, country: str = None) -> str:
+    def get_gadm_gid(
+        self,
+        area: str = None,
+        country: str = None,
+        row: list = None,
+        area_col: str = None,
+        country_col: str = None,
+    ) -> str:
+        if not area and area_col and row: area = row[area_col] 
+        if not country and country_col and row: country = row[country_col]
+
         region, subregion, intermediateregion, iso, united_states = (
             "Region Name",
             "Sub-region Name",
@@ -67,7 +83,8 @@ class NormalizeLoc:
                 .unique()
                 .tolist()
             )
-
+        
+        # limit GADM search to one country
         gadm_df = (
             self.gadm.loc[self.gadm["COUNTRY"] == country] if country else self.gadm
         )
@@ -93,18 +110,39 @@ class NormalizeLoc:
             return [f"{i}:{area.split(',')[0].strip()}" for i in areas]
 
         # handle countries
-        if area in gadm_df["COUNTRY"].to_list():
-            return gadm_df.loc[gadm_df["COUNTRY"] == area]["GID_0"].unique().tolist()
+        if country in gadm_df["COUNTRY"].to_list() and not area:
+            return gadm_df.loc[gadm_df["COUNTRY"] == country]["GID_0"].unique().tolist()
 
-        # handle rest of GADM
-        deepest = []
+        # if trying to get matches in a single country, do fuzzy search 
+        if country and area:
+            unique_area_sets = [
+                gadm_df[f"NAME_{i}"].dropna().unique().tolist()
+                for i in range(1, 6)
+            ]
+            i = 1
+            for area_set in unique_area_sets:
+                closest_match = difflib.get_close_matches(area, area_set, n=1, cutoff=0.65)
+                print(closest_match)
+                if closest_match: return gadm_df.loc[gadm_df[f"NAME_{i}"] == closest_match[0]][f"GID_{i}"].unique().tolist()
+                i += 1
+
+        # handle rest by getting the deepest area GADM (TODO: maybe it should be the largest instead)
+        #deepest = []
         for i in range(1, 6):
             name_col, gid_col = f"NAME_{i}", f"GID_{i}"
             if area in gadm_df[name_col].to_list():
-                deepest = (
-                    gadm_df.loc[gadm_df[name_col] == area][gid_col].unique().tolist()
-                )
-        return deepest
+                return gadm_df.loc[gadm_df[name_col] == area][gid_col].unique().tolist()
+            
+            # clean out additional parts of a location name (like "county" or "city")
+            alt_name = re.sub(r"(county)|(city)", "", area, flags=re.IGNORECASE).strip()
+            if alt_name in gadm_df[name_col].to_list():
+                return gadm_df.loc[gadm_df[name_col] == alt_name][gid_col].unique().tolist()
+                
+                #deepest = (
+                #    gadm_df.loc[gadm_df[name_col] == area][gid_col].unique().tolist()
+                #)
+                #print(deepest)
+        #return deepest
 
     @staticmethod
     def extract_locations(
