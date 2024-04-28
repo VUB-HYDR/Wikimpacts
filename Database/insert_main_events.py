5264db8a136d5433960146a4698631f0be887a9c
Shorouq <94386553+i-be-snek@users.noreply.github.com>
2024-04-28 11:55:43 +0200

    Normalize digit/word numerals to floats in a (min, max) range (#9)
    * ✨ Add number normalization to extract col data
    
    * ➕ Add number normalization related deps
    
    * 🧑‍💻 Install spacy model if missing
    
    * 💡 Add comments to explain the flow (to myself)
    
    * ♻️ Load spacy model in func
    
    * ♻️ Fix formatting + small refactors
    
    * 🐛 Fix inequality function (check for approx)
    
    * ♻️ Refactor code and logic
    
    * ♻️ Refactor label checking
    
    * 💡 Add comments
    
    * 🐛 Fix approx check order
    
    * 🐛 Handle millions/billions/etc
    
    * ✨ Add extracted/normalized min,max,approx to parquet
    
    * 🗃️ Add min,max,approx cols to db
    
    * ⬇️ Downgrade to python3.9
    
    * ⬇️ Downgrade deps for python3.8
    
    * 🏷️ Fix List type
    
    * 🏷️ Fix more list types -> List
    
    * 🏷️ Fix types to run code on python3.8
    
    * 🧑‍💻 Add args to pass to python scripts
    
    * 📝 Add docs
    
    * Fix NormalizeNum instantiation
    
    * Fix string '0' being identified as approximation case
    
    * Fix Indian Rupee normalization
    
    * Fix 'hundreds of' cases
    
    * Fix '>43 total' case
    
    * 🩹 Fix "none" for Total_* cols meaning "zero"
    
    * 🚨 Fix lint + format
    
    * 🎨 Clean out comments
    
    * ⚰️ Remove stats
    
    * ♻️ Add catch-all for approx
    
    * 📦️  Refresh parquet data files and db
    
    ---------
    
    Co-authored-by: Shorouq <shorouq.zahra@ri.se>
    Co-authored-by: chanjuan meng <brionymeng@gmail.com>

--- Database/insert_main_events.py
+++ Database/insert_main_events.py
@@ -1,10 +1,49 @@
+import argparse
 import sqlite3
 
 import pandas as pd
 
 if __name__ == "__main__":
-    data_path = "Database/output"
-    data = pd.read_parquet(f"{data_path}/response_wiki_GPT4_20240327_eventNo_1_8_all_category.parquet")
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-m",
+        "--method",
+        dest="method",
+        default="append",
+        choices=["append", "replace"],
+        help="Choose whether to append data to the exisitng db or to replace it",
+        type=str,
+    )
+
+    parser.add_argument(
+        "-d",
+        "--data_path",
+        dest="data_path",
+        default="Database/output",
+        help="The directory where .parquet files exist so they can be inserted into the db",
+        type=str,
+    )
+
+    parser.add_argument(
+        "-f",
+        "--filename",
+        dest="filename",
+        default="response_wiki_GPT4_20240327_eventNo_1_8_all_category.parquet",
+        help="The name of the parquet file in the <DATA_PATH> directory",
+        type=str,
+    )
+
+    parser.add_argument(
+        "-db",
+        "--database_name",
+        dest="database_name",
+        default="impact.db",
+        help="The name of your database (follwed by `.db`)",
+        type=str,
+    )
+
+    args = parser.parse_args()
+    data = pd.read_parquet(f"{args.data_path}/{args.filename}")
     data = data[
         [
             "Event_ID",
@@ -30,33 +69,54 @@ if __name__ == "__main__":
             "End_Date",
             # "Time_with_Annotation",
             "Total_Deaths",
+            "Total_Deaths_Min",
+            "Total_Deaths_Max",
+            "Total_Deaths_Approx",
             # "Total_Death_Annotation",
             "Total_Injuries",
+            "Total_Injuries_Min",
+            "Total_Injuries_Max",
+            "Total_Injuries_Approx",
             # "Total_Injuries_Annotation",
             "Total_Displacement",
+            "Total_Displacement_Min",
+            "Total_Displacement_Max",
+            "Total_Displacement_Approx",
             # "Total_Displacement_Annotation",
             "Total_Homelessness",
+            "Total_Homelessness_Min",
+            "Total_Homelessness_Max",
+            "Total_Homelessness_Approx",
             # "Total_Homelessness_Annotation",
             "Total_Insured_Damage",
+            "Total_Insured_Damage_Min",
+            "Total_Insured_Damage_Max",
+            "Total_Insured_Damage_Approx",
             "Total_Insured_Damage_Units",
             "Total_Insured_Damage_Inflation_Adjusted",
             "Total_Insured_Damage_Inflation_Adjusted_Year",
             # "Total_Insured_Damage_Assessment_with_annotation",
             "Total_Damage",
+            "Total_Damage_Min",
+            "Total_Damage_Max",
+            "Total_Damage_Approx",
             "Total_Damage_Units",
             "Total_Damage_Inflation_Adjusted",
             "Total_Damage_Inflation_Adjusted_Year",
             # "Economic_Impact_with_annotation",
             "Total_Buildings_Damage",
+            "Total_Buildings_Damage_Min",
+            "Total_Buildings_Damage_Max",
+            "Total_Buildings_Damage_Approx",
             # "Total_Building_Damage_with_annotation"]
         ]
     ]
 
-    connection = sqlite3.connect(f"{data_path}/impact.db")
+    connection = sqlite3.connect(f"{args.data_path}/{args.database_name}")
     cursor = connection.cursor()
 
     # change if_exists to "append" to avoid overwriting the database
     # choose "replace" to overwrite the database with a fresh copy of the data
-    data.to_sql("Events", con=connection, if_exists="replace", index=False)
+    data.to_sql("Events", con=connection, if_exists=args.method, index=False)
 
     connection.close()
