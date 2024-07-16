71d390122b9393416781d5985ca78edaf3285b6a
Shorouq <shorouq.zahra@ri.se>
2024-07-16 16:24:17 +0200

    ✅ Add negative example to unit test

--- Evaluation/tests/test_matcher.py
+++ Evaluation/tests/test_matcher.py
@@ -17,8 +17,22 @@ class TestSpecificInstanceMatcher:
                 ),
                 [0.3333333333333333, 0.6666666666666666],
             ),
+            (
+                (
+                    {"Num_Mix": 0, "Num_Max": 10, "Start_Date_Year": 2030},
+                    [
+                        {"Num_Min": 2, "Num_Max": 91, "Start_Date_Year": 2030},
+                        {"Num_Min": 0, "Num_Max": 10, "Start_Date_Year": 2031},
+                    ],
+                ),
+                None,
+            ),
         ],
     )
     def test_calc_similarity(self, test_input, expected):
         matcher = SpecificInstanceMatcher()
-        assert matcher.calc_similarity(test_input[0], test_input[1]) == expected
+        if expected:
+            assert matcher.calc_similarity(test_input[0], test_input[1]) == expected
+        else:
+            with pytest.raises(KeyError):
+                matcher.calc_similarity(test_input[0], test_input[1])
