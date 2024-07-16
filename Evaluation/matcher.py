04e0e57585b3a8d84b46faf03709e7466619bb69
Shorouq <shorouq.zahra@ri.se>
2024-07-16 16:26:06 +0200

    🍻 Add unfinished code

--- Evaluation/matcher.py
+++ Evaluation/matcher.py
@@ -1,11 +1,13 @@
 from statistics import mean
 
+# from Evaluation.comparer import Comparer
+
 
 class SpecificInstanceMatcher:
     """Matches and pads specific instances (subevents) from two separate lists.
     'Padded' specific instances will have NoneType objects as values"""
 
-    def __init__(self, threshold: float = 0.66):
+    def __init__(self, threshold: float = 0.75):
         self.threshold = threshold
         self.int_cat: list[str] = [
             "Num_Min",
@@ -18,9 +20,7 @@ class SpecificInstanceMatcher:
             "End_Date_Month",
             "End_Date_Year",
         ]  # consider day or not?
-        self.bool_cat: list[str] = [
-            "Specific_Instance_Adjusted"
-        ]  # check name for adjusted
+        self.bool_cat: list[str] = ["Specific_Instance_Adjusted"]  # check name for adjusted
         self.str_cat: list[str] = ["Country_Norm", "Specific_Instance_Unit"]
         self.list_cat: list[str] = ["Location_Norm"]
 
@@ -46,3 +46,41 @@ class SpecificInstanceMatcher:
         # index of mean score corresponds to sys_list item
         return score_list
 
+    def match(self, gold_list: list[dict], sys_list: list[dict]) -> tuple[list[dict]]:
+        gold, sys, sim, already_matched = [], [], [], []  # N, M, M
+
+        for si in gold_list:
+            similarity = self.calc_similarity(si, sys_list)
+            gold.append(si)
+            best_match_index = similarity.index(max(similarity))
+            if max(similarity) >= self.threshold and best_match_index not in already_matched:
+                # returns index of the first max match only!
+                # best_match = sys_list[best_match_index]
+                # sim.append(similarity)  # as long as the sys list
+                already_matched.append(similarity.index(max(similarity)))
+                sys.append(sys_list[best_match_index])
+            else:
+                sys.append(self.create_pad(si))
+
+        # print(sim)
+
+        for i in range(len(gold)):
+            print(gold[i], "--->", sys[i])
+        return (gold, sys)
+
+
+if __name__ == "__main__":
+    matcher = SpecificInstanceMatcher()
+    matcher.match(
+        [
+            {"Num_Min": 2, "Num_Max": 91, "Start_Date_Year": 2030},
+            {"Num_Min": 2, "Num_Max": 91, "Start_Date_Year": 2030},
+            {"Num_Min": 0, "Num_Max": 10, "Start_Date_Year": 2031},
+        ],
+        [
+            {"Num_Min": 0, "Num_Max": 11, "Start_Date_Year": 2031},
+            {"Num_Min": 0, "Num_Max": 11, "Start_Date_Year": 2021},
+            {"Num_Min": 2, "Num_Max": 91, "Start_Date_Year": 2030},
+            {"Num_Min": 7, "Num_Max": 30, "Start_Date_Year": 2030},
+        ],
+    )
