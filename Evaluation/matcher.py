8c4b52701fb38e498974ca388cf077639a57a330
Shorouq <shorouq.zahra@ri.se>
2024-07-17 16:00:46 +0200

    ✨ Create matcher for specific instances

--- Evaluation/matcher.py
+++ Evaluation/matcher.py
@@ -7,7 +7,7 @@ class SpecificInstanceMatcher:
     """Matches and pads specific instances (subevents) from two separate lists.
     'Padded' specific instances will have NoneType objects as values"""
 
-    def __init__(self, threshold: float = 0.75):
+    def __init__(self, threshold: float = 0.6):
         self.threshold = threshold
         self.int_cat: list[str] = [
             "Num_Min",
@@ -57,25 +57,32 @@ class SpecificInstanceMatcher:
         return score_list
 
     def match(self, gold_list: list[dict], sys_list: list[dict]) -> tuple[list[dict]]:
-        gold, sys, sim, already_matched = [], [], [], []  # N, M, M
+        gold, sys, already_matched = [], [], []
 
         for si in gold_list:
-            similarity = self.calc_similarity(si, sys_list)
             gold.append(si)
-            best_match_index = similarity.index(max(similarity))
-            if max(similarity) >= self.threshold and best_match_index not in already_matched:
-                # returns index of the first max match only!
-                # best_match = sys_list[best_match_index]
-                # sim.append(similarity)  # as long as the sys list
-                already_matched.append(similarity.index(max(similarity)))
-                sys.append(sys_list[best_match_index])
-            else:
-                sys.append(self.create_pad(si))
+            similarity = self.calc_similarity(si, sys_list)
 
-        # print(sim)
+            # create a list of indices of the matches sorted by value (best matches first)
+            top_matches_sorted_indices = [i for i, x in sorted(enumerate(similarity), key=lambda x: x[1], reverse=True)]
 
-        for i in range(len(gold)):
-            print(gold[i], "--->", sys[i])
+            for i in top_matches_sorted_indices:
+                # inspect the best matches from the top
+                # match if a specific instance meets the threshold & has not already been matched by another specific instance
+                if similarity[i] >= self.threshold and similarity[i] not in already_matched:
+                    already_matched.append(similarity[i])
+                    sys.append(sys_list[i])
+                    break
+                else:
+                    # otherwise, create a "padded" match
+                    sys.append(self.create_pad(si))
+
+        # if any events in the sys output remain unmatched, create a "padded" match for them
+        for si in sys_list:
+            if si not in sys:
+                sys.append(si)
+                gold.append(self.create_pad(si))
+
         return (gold, sys)
 
 
@@ -83,14 +90,49 @@ if __name__ == "__main__":
     matcher = SpecificInstanceMatcher()
     matcher.match(
         [
-            {"Num_Min": 2, "Num_Max": 91, "Start_Date_Year": 2030},
-            {"Num_Min": 2, "Num_Max": 91, "Start_Date_Year": 2030},
-            {"Num_Min": 0, "Num_Max": 10, "Start_Date_Year": 2031},
+            {
+                "Num_Min": 2,
+                "Num_Max": 82,
+                "Start_Date_Year": 2030,
+                "Location_Norm": ["Amman", "Zarqa"],
+            },
+            {
+                "Num_Min": 2,
+                "Num_Max": 91,
+                "Start_Date_Year": 2030,
+                "Location_Norm": ["Uppsala", "Stockholm"],
+            },
+            {
+                "Num_Min": 0,
+                "Num_Max": 10,
+                "Start_Date_Year": 2031,
+                "Location_Norm": ["Paris", "Lyon"],
+            },
         ],
         [
-            {"Num_Min": 0, "Num_Max": 11, "Start_Date_Year": 2031},
-            {"Num_Min": 0, "Num_Max": 11, "Start_Date_Year": 2021},
-            {"Num_Min": 2, "Num_Max": 91, "Start_Date_Year": 2030},
-            {"Num_Min": 7, "Num_Max": 30, "Start_Date_Year": 2030},
+            {
+                "Num_Min": 0,
+                "Num_Max": 11,
+                "Start_Date_Year": 2031,
+                "Location_Norm": ["Lyon"],
+            },
+            {
+                "Num_Min": 1,
+                "Num_Max": 84,
+                "Start_Date_Year": 2029,
+                "Location_Norm": ["Uppsala", "Zarqa"],
+            },
+            {
+                "Num_Min": 2,
+                "Num_Max": 91,
+                "Start_Date_Year": 2030,
+                "Location_Norm": ["Stockholm"],
+            },
+            {
+                "Num_Min": 7,
+                "Num_Max": 30,
+                "Start_Date_Year": 2030,
+                "Location_Norm": ["Uppsala", "Linköping"],
+            },
         ],
     )
