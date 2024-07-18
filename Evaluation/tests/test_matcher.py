6bc3eb6a5dcac7122344529f9e363e81ba610e29
Shorouq <shorouq.zahra@ri.se>
2024-07-18 08:59:20 +0200

    ✅ Update similarity calc test with clearer param names

--- Evaluation/tests/test_matcher.py
+++ Evaluation/tests/test_matcher.py
@@ -5,37 +5,33 @@ from Evaluation.matcher import SpecificInstanceMatcher
 
 class TestSpecificInstanceMatcher:
     @pytest.mark.parametrize(
-        "test_input, expected",
+        "gold_instance, sys_list, expected",
         [
             (
-                (
-                    {"Num_Min": 0, "Num_Max": 10, "Start_Date_Year": 2030},
-                    [
-                        {"Num_Min": 2, "Num_Max": 91, "Start_Date_Year": 2030},
-                        {"Num_Min": 0, "Num_Max": 10, "Start_Date_Year": 2031},
-                    ],
-                ),
+                {"Num_Min": 0, "Num_Max": 10, "Start_Date_Year": 2030},
+                [
+                    {"Num_Min": 2, "Num_Max": 91, "Start_Date_Year": 2030},
+                    {"Num_Min": 0, "Num_Max": 10, "Start_Date_Year": 2031},
+                ],
                 [0.39933993399339934, 0.9999179184109004],
             ),
             (
-                (
-                    {"Num_Mix": 0, "Num_Max": 10, "Start_Date_Year": 2030},
-                    [
-                        {"Num_Min": 2, "Num_Max": 91, "Start_Date_Year": 2030},
-                        {"Num_Min": 0, "Num_Max": 10, "Start_Date_Year": 2031},
-                    ],
-                ),
+                {"Num_Mix": 0, "Num_Max": 10, "Start_Date_Year": 2030},
+                [
+                    {"Num_Min": 2, "Num_Max": 91, "Start_Date_Year": 2030},
+                    {"Num_Min": 0, "Num_Max": 10, "Start_Date_Year": 2031},
+                ],
                 None,
             ),
         ],
     )
-    def test_calc_similarity(self, test_input, expected):
+    def test_calc_similarity(self, gold_instance, sys_list, expected):
         matcher = SpecificInstanceMatcher()
         if expected:
-            assert matcher.calc_similarity(test_input[0], test_input[1]) == expected
+            assert matcher.calc_similarity(gold_instance, sys_list) == expected
         else:
             with pytest.raises(UnboundLocalError):
-                matcher.calc_similarity(test_input[0], test_input[1])
+                matcher.calc_similarity(gold_instance, sys_list)
 
     @pytest.mark.parametrize(
         "test_gold_list, test_sys_list, expected_gold, expected_sys",
