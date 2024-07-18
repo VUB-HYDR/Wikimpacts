0d77572b1c3e385913cd0c74a8ecda3f8123951f
Shorouq <shorouq.zahra@ri.se>
2024-07-18 16:14:52 +0200

    🥅 Catch unsupported columns with warning

--- Evaluation/matcher.py
+++ Evaluation/matcher.py
@@ -52,11 +52,9 @@ class SpecificInstanceMatcher:
                 elif k in self.list_cat:
                     r = self.comp.sequence(gold_instance[k], si[k])
                 try:
-                    r
-                except UnboundLocalError:
-                    print(f"Unsupported column name: {k}")
-                    raise UnboundLocalError
-                scores.append(1 - r)
+                    scores.append(1 - r)
+                except UserWarning as err:
+                    self.logger.warning(f"Unsupported column name: {k}. Error message: {err}")
 
             score_list.append(mean(scores))
 
