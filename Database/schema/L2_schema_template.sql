0678704b7ebca520c317596a8ff461c73dc04db3
Shorouq <shorouq.zahra@ri.se>
2024-09-09 00:14:25 +0200

    🗃️ Fix validation for db insertion

--- Database/schema/L2_schema_template.sql
+++ Database/schema/L2_schema_template.sql
@@ -5,12 +5,12 @@ CREATE TABLE Instance_Per_Administrative_Areas_type_numerical (
     Administrative_Areas_Type OBJECT, /* COMMENT 'Array' of TEXT/NULL */
     Administrative_Areas_GeoJson OBJECT, /* COMMENT 'Array' of GeoJson Objects or NULLs */
 
-    Start_Date_Day INTEGER CHECK (Start_Date_Day <= 31),
-    Start_Date_Month INTEGER CHECK (Start_Date_Month <= 12),
-    Start_Date_Year INTEGER CHECK (length(Start_Date_Year) = 4),
-    End_Date_Day INTEGER CHECK (End_Date_Day <= 31),
-    End_Date_Month INTEGER CHECK (End_Date_Month <= 12),
-    End_Date_Year INTEGER CHECK (length(End_Date_Year) = 4),
+    Start_Date_Day INTEGER CHECK (Start_Date_Day <= 31 OR Start_Date_Day == NULL),
+    Start_Date_Month INTEGER CHECK (Start_Date_Month <= 12 OR Start_Date_Month == NULL),
+    Start_Date_Year INTEGER NOT NULL CHECK (length(Start_Date_Year) == 4 OR Start_Date_Year == NULL),
+    End_Date_Day INTEGER CHECK (End_Date_Day <= 31 OR End_Date_Day == NULL),
+    End_Date_Month INTEGER CHECK (End_Date_Month <= 12 OR End_Date_Month == NULL),
+    End_Date_Year INTEGER NOT NULL CHECK (length(End_Date_Year) == 4),
 
     Num_Min REAL NOT NULL CHECK (Num_Min > 0),
     Num_Max REAL NOT NULL CHECK (Num_Max > 0),
@@ -26,12 +26,12 @@ CREATE TABLE Instance_Per_Administrative_Areas_type_monetary (
     Administrative_Areas_Type OBJECT, /* COMMENT 'Array' of TEXT/NULL */
     Administrative_Areas_GeoJson OBJECT, /* COMMENT 'Array' of GeoJson Objects or NULLs */
 
-    Start_Date_Day INTEGER CHECK (Start_Date_Day <= 31),
-    Start_Date_Month INTEGER CHECK (Start_Date_Month <= 12),
-    Start_Date_Year INTEGER CHECK (length(Start_Date_Year) = 4),
-    End_Date_Day INTEGER CHECK (End_Date_Day <= 31),
-    End_Date_Month INTEGER CHECK (End_Date_Month <= 12),
-    End_Date_Year INTEGER CHECK (length(End_Date_Year) = 4),
+    Start_Date_Day INTEGER CHECK (Start_Date_Day <= 31 OR Start_Date_Day == NULL),
+    Start_Date_Month INTEGER CHECK (Start_Date_Month <= 12 OR Start_Date_Month == NULL),
+    Start_Date_Year INTEGER NOT NULL CHECK (length(Start_Date_Year) == 4 OR Start_Date_Year == NULL),
+    End_Date_Day INTEGER CHECK (End_Date_Day <= 31 OR End_Date_Day == NULL),
+    End_Date_Month INTEGER CHECK (End_Date_Month <= 12 OR End_Date_Month == NULL),
+    End_Date_Year INTEGER NOT NULL CHECK (length(End_Date_Year) == 4 OR End_Date_Year == NULL),
 
     Num_Min REAL NOT NULL CHECK (Num_Min > 0),
     Num_Max REAL NOT NULL CHECK (Num_Max > 0),
@@ -39,7 +39,8 @@ CREATE TABLE Instance_Per_Administrative_Areas_type_monetary (
     Num_Unit TEXT NOT NULL, /* COMMENT 'currency' */
     Num_Inflation_Adjusted INTEGER, /* COMMENT 'Boolean' */
     Num_Inflation_Adjusted_Year INTEGER CHECK (
-        length(Num_Inflation_Adjusted_Year) = 4
+        length(Num_Inflation_Adjusted_Year) == 4
+        OR Num_Inflation_Adjusted_Year == NULL
     ),
 
     FOREIGN KEY(Event_ID) REFERENCES Total_Summary_Events(Event_ID)
--- Database/schema/L3_schema_template.sql
+++ Database/schema/L3_schema_template.sql
@@ -11,12 +11,13 @@ CREATE TABLE Specific_Instance_Per_Administrative_Area_type_numerical (
     Locations_Type OBJECT, /* COMMENT 'Array' of TEXT/NULL, categorical */
     Locations_GeoJson OBJECT, /* COMMENT 'Array' of GeoJson Objects or NULLs */
 
-    Start_Date_Day INTEGER CHECK (Start_Date_Day <= 31),
-    Start_Date_Month INTEGER CHECK (Start_Date_Month <= 12),
-    Start_Date_Year INTEGER CHECK (length(Start_Date_Year) = 4),
-    End_Date_Day INTEGER CHECK (End_Date_Day <= 31),
-    End_Date_Month INTEGER CHECK (End_Date_Month <= 12),
-    End_Date_Year INTEGER CHECK (length(End_Date_Year) = 4),
+    Start_Date_Day INTEGER CHECK (Start_Date_Day <= 31 OR Start_Date_Day == NULL),
+    Start_Date_Month INTEGER CHECK (Start_Date_Month <= 12 OR Start_Date_Month == NULL),
+    Start_Date_Year INTEGER NOT NULL CHECK (length(Start_Date_Year) == 4 OR Start_Date_Year == NULL),
+    End_Date_Day INTEGER CHECK (End_Date_Day <= 31 OR End_Date_Day == NULL),
+    End_Date_Month INTEGER CHECK (End_Date_Month <= 12 OR End_Date_Month == NULL),
+    End_Date_Year INTEGER NOT NULL CHECK (length(End_Date_Year) == 4 OR End_Date_Year == NULL),
+
 
     Num_Min REAL CHECK (Num_Min > 0),
     Num_Max REAL CHECK (Num_Max > 0),
@@ -38,12 +39,12 @@ CREATE TABLE Specific_Instance_Per_Administrative_Area_type_monetary (
     Locations_Type OBJECT, /* COMMENT 'Array' of TEXT/NULL, categorical */
     Locations_GeoJson OBJECT, /* COMMENT 'Array' of GeoJson Objects or NULLs */
 
-    Start_Date_Day INTEGER CHECK (Start_Date_Day <= 31),
-    Start_Date_Month INTEGER CHECK (Start_Date_Month <= 12),
-    Start_Date_Year INTEGER NOT NULL CHECK (length(Start_Date_Year) = 4),
-    End_Date_Day INTEGER CHECK (End_Date_Day <= 31),
-    End_Date_Month INTEGER NOT NULL CHECK (End_Date_Month <= 12),
-    End_Date_Year INTEGER NOT NULL CHECK (length(End_Date_Year) = 4),
+    Start_Date_Day INTEGER CHECK (Start_Date_Day <= 31 OR Start_Date_Day == NULL),
+    Start_Date_Month INTEGER CHECK (Start_Date_Month <= 12 OR Start_Date_Month == NULL),
+    Start_Date_Year INTEGER NOT NULL CHECK (length(Start_Date_Year) == 4 OR Start_Date_Year == NULL),
+    End_Date_Day INTEGER CHECK (End_Date_Day <= 31 OR End_Date_Day == NULL),
+    End_Date_Month INTEGER CHECK (End_Date_Month <= 12 OR End_Date_Month == NULL),
+    End_Date_Year INTEGER NOT NULL CHECK (length(End_Date_Year) == 4),
 
     Num_Min REAL NOT NULL CHECK (Num_Min > 0),
     Num_Max REAL NOT NULL CHECK (Num_Max > 0),
@@ -51,7 +52,8 @@ CREATE TABLE Specific_Instance_Per_Administrative_Area_type_monetary (
     Num_Unit TEXT NOT NULL, /* COMMENT 'currency' */
     Num_Inflation_Adjusted INTEGER, /* COMMENT 'Boolean' */
     Num_Inflation_Adjusted_Year INTEGER CHECK (
-        length(Num_Inflation_Adjusted_Year) = 4
+        length(Num_Inflation_Adjusted_Year) == 4
+        OR Num_Inflation_Adjusted_Year == NULL
     ),
 
     FOREIGN KEY(Event_ID) REFERENCES Total_Summary_Events(Event_ID)
