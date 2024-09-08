5f2f661c99389fce3dd00e02da81418c5798f30a
Shorouq <shorouq.zahra@ri.se>
2024-09-08 16:45:05 +0200

    🔧 Update weights config (unified column names)

--- Evaluation/weights.py
+++ Evaluation/weights.py
@@ -1,26 +1,27 @@
-weights = {
-    "all_columns": {
+default_weights = {
+    "l1_columns": {
         "Event_ID": 1,
         "Main_Event": 1,
         "Event_Name": 0,
+        "Hazards": 1,
         # Deaths
         "Total_Deaths_Min": 1,
         "Total_Deaths_Max": 1,
         # Injuries
         "Total_Injuries_Max": 1,
         "Total_Injuries_Min": 1,
-        # Buildings_Damage
-        "Total_Buildings_Min": 1,
-        "Total_Buildings_Max": 1,
+        # Buildings Damaged
+        "Total_Buildings_Damaged_Min": 1,
+        "Total_Buildings_Damaged_Max": 1,
         # Affected
         "Total_Affected_Min": 1,
         "Total_Affected_Max": 1,
         # Homeless
         "Total_Homeless_Min": 1,
         "Total_Homeless_Max": 1,
-        # Displace
-        "Total_Displace_Min": 1,
-        "Total_Displace_Max": 1,
+        # Displaced
+        "Total_Displaced_Min": 1,
+        "Total_Displaced_Max": 1,
         # Damage
         "Total_Damage_Min": 1,
         "Total_Damage_Max": 1,
@@ -40,10 +41,51 @@ weights = {
         "End_Date_Day": 1,
         "End_Date_Month": 1,
         "End_Date_Year": 1,
-        # Location
-        "Country_Norm": 1,
-        "Location_Norm": 1,
+        # Area
+        "Administrative_Areas_Norm": 1,  # list
     },
+    "l2_columns": {
+        "Event_ID": 1,
+        "Hazards": 1,
+        # Date
+        "Start_Date_Day": 1,
+        "Start_Date_Month": 1,
+        "Start_Date_Year": 1,
+        "End_Date_Day": 1,
+        "End_Date_Month": 1,
+        "End_Date_Year": 1,
+        # Area
+        "Administrative_Areas_Norm": 1,  # list
+        # Impact
+        "Num_Min": 1,
+        "Num_Max": 1,
+        "Num_Unit": 1,
+        "Num_Inflation_Adjusted": 1,
+        "Num_Inflation_Adjusted_Year": 1,
+    },
+    "l3_columns": {
+        "Event_ID": 1,
+        "Hazards": 1,
+        # Date
+        "Start_Date_Day": 1,
+        "Start_Date_Month": 1,
+        "Start_Date_Year": 1,
+        "End_Date_Day": 1,
+        "End_Date_Month": 1,
+        "End_Date_Year": 1,
+        # Area
+        "Administrative_Area_Norm": 1,  # str
+        "Locations_Norm": 1,  # list
+        # Impact
+        "Num_Min": 1,
+        "Num_Max": 1,
+        "Num_Unit": 1,
+        "Num_Inflation_Adjusted": 1,
+        "Num_Inflation_Adjusted_Year": 1,
+    },
+}
+
+weights = {
     "nlp4climate": {
         "Event_ID": 1,
         "Main_Event": 1,
@@ -67,49 +109,7 @@ weights = {
         "Total_Homeless_Min": 1,
         "Total_Homeless_Max": 1,
     },
-    "ESSD_2024": {
-        "Event_ID": 1,
-        "Main_Event": 1,
-        "Event_Name": 0,
-        # Deaths
-        "Total_Deaths_Min": 1,
-        "Total_Deaths_Max": 1,
-        # Injuries
-        "Total_Injuries_Max": 1,
-        "Total_Injuries_Min": 1,
-        # Buildings_Damage
-        "Total_Buildings_Min": 1,
-        "Total_Buildings_Max": 1,
-        # Affected
-        "Total_Affected_Min": 1,
-        "Total_Affected_Max": 1,
-        # Homeless
-        "Total_Homeless_Min": 1,
-        "Total_Homeless_Max": 1,
-        # Displace
-        "Total_Displace_Min": 1,
-        "Total_Displace_Max": 1,
-        # Damage
-        "Total_Damage_Min": 1,
-        "Total_Damage_Max": 1,
-        "Total_Damage_Units": 1,
-        "Total_Damage_Inflation_Adjusted": 1,
-        "Total_Damage_Inflation_Adjusted_Year": 1,
-        # Insured Damage
-        "Total_Insured_Damage_Min": 1,
-        "Total_Insured_Damage_Max": 1,
-        "Total_Insured_Damage_Units": 1,
-        "Total_Insured_Damage_Inflation_Adjusted": 1,
-        "Total_Insured_Damage_Inflation_Adjusted_Year": 1,
-        # Date
-        "Start_Date_Day": 1,
-        "Start_Date_Month": 1,
-        "Start_Date_Year": 1,
-        "End_Date_Day": 1,
-        "End_Date_Month": 1,
-        "End_Date_Year": 1,
-        # Location
-        # "Country_Norm": 1,
-        # "Location_Norm": 1,
-    },
+    "ESSD_2024_l1": default_weights["l1"],
+    "ESSD_2024_l2": default_weights["l2"],
+    "ESSD_2024_l3": default_weights["l3"],
 }
