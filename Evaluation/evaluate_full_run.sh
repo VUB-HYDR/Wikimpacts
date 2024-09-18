79b82fc0f384c2e110527470611ee4a1675bc0f2
Shorouq <shorouq.zahra@ri.se>
2024-09-18 10:32:49 +0200

    🔨 Add docs to evaluation full run bash script

--- Evaluation/evaluate_full_run.sh
+++ Evaluation/evaluate_full_run.sh
@@ -1,6 +1,71 @@
 #!/usr/bin/env bash
 
-levels=("l2" "l3") # l1
+##################
+# This bash script evaluates all event levels (l1, l2, and l3) in one go
+# The goldFileDir and sysFileDir must exist in a certain tree format as shown below:
+
+# GOLD FILE DIR
+# ├── l1
+# │   └── Total_Summary.parquet
+# ├── l2
+# │   ├── Instance_Per_Administrative_Areas_Affected.parquet
+# │   ├── Instance_Per_Administrative_Areas_Buildings_Damaged.parquet
+# │   ├── Instance_Per_Administrative_Areas_Damage.parquet
+# │   ├── Instance_Per_Administrative_Areas_Deaths.parquet
+# │   ├── Instance_Per_Administrative_Areas_Displaced.parquet
+# │   ├── Instance_Per_Administrative_Areas_Homeless.parquet
+# │   └── Instance_Per_Administrative_Areas_Injuries.parquet
+# └── l3
+#     ├── Specific_Instance_Per_Administrative_Area_Affected.parquet
+#     ├── Specific_Instance_Per_Administrative_Area_Buildings_Damaged.parquet
+#     ├── Specific_Instance_Per_Administrative_Area_Damage.parquet
+#     ├── Specific_Instance_Per_Administrative_Area_Deaths.parquet
+#     ├── Specific_Instance_Per_Administrative_Area_Displaced.parquet
+#     ├── Specific_Instance_Per_Administrative_Area_Homeless.parquet
+#     ├── Specific_Instance_Per_Administrative_Area_Injuries.parquet
+#     └── Specific_Instance_Per_Administrative_Area_Insured_Damage.parquet
+#
+# SYS FILE DIR
+# .
+# ├── l1
+# │   └── 0000.parquet
+# ├── l2
+# │   ├── Instance_Per_Administrative_Areas_Affected
+# │   │   ├── 0000.parquet
+# │   │   ├── 0001.parquet
+# │   │   ├── ...
+# │   │   └── 0034.parquet
+# │   ├── Instance_Per_Administrative_Areas_Buildings_Damaged
+# │   │   └── 0000.parquet
+# │   ├── Instance_Per_Administrative_Areas_Displaced
+# │   │   ├── 0000.parquet
+# │   │   ├── 0001.parquet
+# │   │   ├── ...
+# │   │   └── 0201.parquet
+# │   └── Instance_Per_Administrative_Areas_Damage
+# │       ├── 0000.parquet
+# │       ├── 0001.parquet
+# │       ├── ...
+# │       └── 0345.parquet
+# └── l3
+#     ├── Specific_Instance_Per_Administrative_Area_Affected
+#     │   ├── 0000.parquet
+#     │   └── 0001.parquet
+#     ├── Specific_Instance_Per_Administrative_Area_Buildings_Damaged
+#     │   ├── 0000.parquet
+#     │   ├── 0001.parquet
+#     │   └── 0002.parquet
+#     ├── Specific_Instance_Per_Administrative_Area_Deaths
+#     │   ├── 0000.parquet
+#     │   ├── 0001.parquet
+#     │   ├── ...
+#     │   └── 0103.parquet
+#     └── Specific_Instance_Per_Administrative_Area_Injuries
+#         └── 0000.parquet
+#
+###################
+
+levels=("l1" "l2" "l3")
 monetary_impacts=("Damage" "Insured_Damage")
 numerical_impacts=("Deaths" "Injuries" "Affected" "Buildings_Damaged" "Homeless" "Displaced")
 goldFileDir=${1}
