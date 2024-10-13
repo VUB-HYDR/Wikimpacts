55174601c0d14cf7e0d9068c87c9eee162af2561
Shorouq <shorouq.zahra@ri.se>
2024-10-13 18:56:49 +0200

    🔨 Fix json inconsistencies as part of parsing full run

--- Database/parse_full_run.sh
+++ Database/parse_full_run.sh
@@ -1,23 +1,29 @@
 #!/usr/bin/env bash
 
 inputFilesDir=${1}
-outputFilesDir=${1}
+outputFilesDir=${2}
 
 echo Input dir: ${inputFilesDir}
 mkdir ${outputFilesDir}
 echo Parsing files to output dir: ${outputFilesDir}
 
 jsonChunks=(${inputFilesDir}/*)
-
+prefix=".fixed.json"
 lenJsonChunks=${#jsonChunks[@]}
 echo "Found ${lenJsonChunks} chunks"
 
 for chunk in "${jsonChunks[@]}"; do
-    echo "Parsing ${chunk}"
+
+    echo Fixing chunk ${chunk} for list of nums
+    poetry run python3 Database/fix_json_inconsistencies.py -i ${chunk} -o ${chunk}${prefix} -n "data types and nested fields"
+    echo Fixing chunk ${chunk} data types and nested fields
+    poetry run python3 Database/fix_json_inconsistencies.py -i ${chunk}${prefix} -o ${chunk}${prefix} -n "list of nums"
+    echo Parsing chunk ${chunk}${prefix}
+
     poetry run python3 Database/parse_events.py \
-    --raw_dir ${inputFilesDir} \
-    --filename $(basename ${chunk}) \
-    --output_dir ${outputFilesDir} \
-    --event_levels l1,l2,l3 \
-    || echo "Parsing ${chunk}" has failed!!
+        --raw_dir ${inputFilesDir} \
+        --filename $(basename ${chunk}${prefix}) \
+        --output_dir ${outputFilesDir} \
+        --event_levels l1,l2,l3 ||
+        echo Parsing ${chunk}${prefix} has failed!!
 done
