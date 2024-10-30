3ccf223f5c31545b1da874952aea4aa129aaf7e0
Shorouq <shorouq.zahra@ri.se>
2024-10-30 19:24:22 +0100

    🚧 Add data gap script

---
+++ Database/fill_data_gap.py
@@ -0,0 +1,29 @@
+import argparse
+
+from Database.scr.normalize_data import DataGapUtils
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-i",
+        "--input_dir",
+        type=str,
+        dest="input_dir",
+        help="Provide the llm output directory",
+    )
+
+    args = parser.parse_args()
+    dg_util = DataGapUtils()
+    l1, l2, l3 = dg_util.load_data(input_dir=args.input_dir)
+    event_id, date_split = "Event_ID", "_Date_"
+
+    for event_id in list(l1[event_id].unique()):
+        replace_with_date = (
+            l1.loc[l1[event_id] == event_id][[x for x in l1.columns if date_split in x]].iloc[0].to_dict()
+        )
+
+        for level in [l2, l3]:
+            for impact in level.keys():
+                level[impact][level[impact][event_id] == event_id] = level[impact][
+                    level[impact][event_id] == event_id
+                ].apply(lambda row: dg_util.fill_date(row, replace_with_date=replace_with_date), axis=1)
--- Database/scr/normalize_data.py
+++ Database/scr/normalize_data.py
@@ -1,9 +1,57 @@
+import pandas as pd
+
 from .log_utils import Logging
 
 
 class DataGapUtils:
     def __init__(self):
         self.logger = Logging.get_logger("data-gap-utils")
+
+    def load_data(self, input_dir: str) -> tuple[pd.DataFrame, dict[str, pd.DataFrame], dict[str, pd.DataFrame]]:
+        import os
+
+        from tqdm import tqdm
+
+        from Database.scr.normalize_utils import NormalizeUtils
+
+        norm_utils = NormalizeUtils()
+
+        l1_filename = f"{input_dir}/l1"
+        l2_filenames = os.listdir(f"{input_dir}/l2")
+        l3_filenames = os.listdir(f"{input_dir}/l3")
+
+        l2_categories = [f'{x.split("_Areas_")[-1]}' for x in l2_filenames]
+        l3_categories = [f'{x.split("_Area_")[-1]}' for x in l3_filenames]
+
+        l1 = pd.read_parquet(l1_filename, engine="fastparquet")
+        l1 = norm_utils.replace_nulls(l1).replace({float("nan"): None})
+        l2 = {}
+        self.logger.info("Loading l2 files...")
+
+        for f, c in tqdm(zip(l2_filenames, l2_categories), desc="L2 files..."):
+            try:
+                tmp_df = pd.read_parquet(f"{input_dir}/l2/{f}", engine="fastparquet")
+                tmp_df = norm_utils.replace_nulls(tmp_df).replace({float("nan"): None})
+
+                l2[c] = tmp_df
+                del tmp_df
+
+            except BaseException as err:
+                self.logger.error(f"Could not read {input_dir}/l2/{f}. Error: {err}")
+        l3 = {}
+        self.logger.info("Loading l3 files...")
+        for f, c in tqdm(zip(l3_filenames, l3_categories), desc="L3 files..."):
+            try:
+                tmp_df = pd.read_parquet(f"{input_dir}/l3/{f}", engine="fastparquet")
+                tmp_df = norm_utils.replace_nulls(tmp_df).replace({float("nan"): None})
+
+                l3[c] = tmp_df
+                del tmp_df
+
+            except BaseException as err:
+                self.logger.error(f"Could not read {input_dir}/l3/{f}. Error: {err}")
+
+        return l1, l2, l3
 
     @staticmethod
     def fill_date(row: dict, replace_with_date: dict):
@@ -12,3 +60,11 @@ class DataGapUtils:
             for c in date_cols:
                 row[c] = replace_with_date[c]
         return row
+
+    @staticmethod
+    def fill_area(row: dict, replace_with_area: dict):
+        pass
+
+    @staticmethod
+    def fill_count(row: dict, replace_with_count: dict):
+        pass
