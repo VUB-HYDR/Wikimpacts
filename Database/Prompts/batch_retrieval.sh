0d903aa060b05d02cfdc8636569abd1adb08aca6
vsc10684 <vsc10684@node300.hydra.os>
2025-06-26 16:08:55 +0200

    update all multi events full run

--- Database/Prompts/batch_retrieval.sh
+++ Database/Prompts/batch_retrieval.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
-counter=1
+counter=21
+count=0
 for file in Database/Wikipedia_articles/multi_events_full_run/*.json; do
+ 
+  count=$((count + 1))
+  if [ "$count" -lt 21 ]; then
+    continue
+  fi
   filename=$(basename "$file")
   poetry run python3 Database/Prompts/batch_output_retrivel.py\
     -r Database/Wikipedia_articles/multi_events_full_run \
@@ -8,7 +14,7 @@ for file in Database/Wikipedia_articles/multi_events_full_run/*.json; do
     --api_env .env \
     -t multi \
     -m o3-mini-2025-01-31\
-    -o Database/output/WikimpactsV2/fullrun/multi_events\
+    -o Database/raw/WikimpactsV2/fullrun/multi_events\
     -d "multi_events_full_run$counter"
   ((counter++))
 done
