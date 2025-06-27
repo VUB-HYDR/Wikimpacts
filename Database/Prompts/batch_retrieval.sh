2240c3fd104e7c7082171ce1286e334b0cc8cab3
vsc10684 <vsc10684@node300.hydra.os>
2025-06-27 08:03:42 +0200

    update the single events full run

--- Database/Prompts/batch_retrieval.sh
+++ Database/Prompts/batch_retrieval.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
-counter=21
+counter=3
 count=0
-for file in Database/Wikipedia_articles/multi_events_full_run/*.json; do
+for file in Database/Wikipedia_articles/full_run_single_events_2.0/*.json; do
  
   count=$((count + 1))
-  if [ "$count" -lt 21 ]; then
+  if [ "$count" -lt 3 ]; then
     continue
   fi
   filename=$(basename "$file")
   poetry run python3 Database/Prompts/batch_output_retrivel.py\
-    -r Database/Wikipedia_articles/multi_events_full_run \
+    -r Database/Wikipedia_articles/full_run_single_events_2.0 \
     -f "$filename" \
     --api_env .env \
-    -t multi \
+    -t single \
     -m o3-mini-2025-01-31\
-    -o Database/raw/WikimpactsV2/fullrun/multi_events\
-    -d "multi_events_full_run$counter"
+    -o Database/raw/WikimpactsV2/fullrun/single_events\
+    -d "single_event_full_run$counter"
   ((counter++))
 done
