95a52f81bab376bb4326ffeefa225a2f359b8adb
vsc10684 <vsc10684@node300.hydra.os>
2025-07-02 08:50:26 +0200

    update rest single events parsed output

--- Database/parse_full_run_v2.sh
+++ Database/parse_full_run_v2.sh
@@ -3,7 +3,7 @@ count=0
 for file in Database/raw/WikimpactsV2/fullrun/single_events/*.json; do 
    
     count=$((count + 1))
-    if [ "$count" -lt 4 ]; then
+    if [ "$count" -lt 7 ]; then
         continue
     fi
     filename=$(basename "$file")
@@ -12,7 +12,7 @@ for file in Database/raw/WikimpactsV2/fullrun/single_events/*.json; do
     poetry run python3 Database/parse_events.py \
         -r Database/raw/WikimpactsV2/fullrun/single_events \
         -f "$filename" \
-        -o Database/output/WikimpactsV2/fullrun/single_event_1 \
+        -o Database/output/WikimpactsV2/fullrun/single_event_2 \
         -lvl l1,l3 \
        # echo Parsing $filename has failed!!
 done
