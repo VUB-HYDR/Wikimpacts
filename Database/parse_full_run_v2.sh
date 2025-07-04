ffa84ff5522eea10bf44f9364e2210007dff8169
vsc10684 <vsc10684@node300.hydra.os>
2025-07-04 14:03:37 +0200

    update the parse full run code for table and list

--- Database/parse_full_run_v2.sh
+++ Database/parse_full_run_v2.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 count=0
-for file in Database/raw/WikimpactsV2/fullrun/multi_events/*.json; do 
+for file in Database/raw/WikimpactsV2/fullrun/multi_events/table_list/filter/*.json; do 
    
-    count=$((count + 1))
-    if [ "$count" -lt 21 ]; then
+    #count=$((count + 1))
+    #if [ "$count" -lt 21 ]; then
         continue
-    fi
+    #fi
     filename=$(basename "$file")
     echo file name: $filename
     
     poetry run python3 Database/parse_events.py \
         -r Database/raw/WikimpactsV2/fullrun/multi_events\
         -f "$filename" \
-        -o Database/output/WikimpactsV2/fullrun/multi_events_1 \
+        -o Database/output/WikimpactsV2/fullrun/multi_events/table_list \
         -lvl l1,l3 \
        # echo Parsing $filename has failed!!
 done
