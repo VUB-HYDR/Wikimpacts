0304f5bc6552a89b341eddf275efa0ac51b15df8
vsc10684 <vsc10684@node300.hydra.os>
2025-07-09 15:01:08 +0200

    update two table file parsed output from multi events

--- Database/parse_full_run_v2.sh
+++ Database/parse_full_run_v2.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for file in Database/raw/WikimpactsV2/fullrun/multi_events/table_list/filter/*.json; do 
+for file in Database/raw/WikimpactsV2/fullrun/multi_events/table_list/filter/4_10/*.json; do 
    
     #count=$((count + 1))
     #if [ "$count" -lt 21 ]; then
@@ -10,9 +10,9 @@ for file in Database/raw/WikimpactsV2/fullrun/multi_events/table_list/filter/*.j
     echo file name: $filename
     
     poetry run python3 Database/parse_events.py \
-        -r Database/raw/WikimpactsV2/fullrun/multi_events/table_list/filter\
+        -r Database/raw/WikimpactsV2/fullrun/multi_events/table_list/filter/4_10\
         -f "$filename" \
-        -o Database/output/WikimpactsV2/fullrun/multi_events/table_list \
+        -o Database/output/WikimpactsV2/fullrun/multi_events/table_list_4_10 \
         -lvl l1,l3 \
        # echo Parsing $filename has failed!!
 done
