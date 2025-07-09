c1b105038129fbd651369ff14ed29a72f15ee93c
vsc10684 <vsc10684@node300.hydra.os>
2025-07-09 15:35:59 +0200

    update results for one file for multi events

--- Database/parse_full_run_v2.sh
+++ Database/parse_full_run_v2.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for file in Database/raw/WikimpactsV2/fullrun/multi_events/table_list/filter/4_10/*.json; do 
+for file in Database/raw/WikimpactsV2/fullrun/multi_events/table_list/filter/4_2/*.json; do 
    
     #count=$((count + 1))
     #if [ "$count" -lt 21 ]; then
@@ -10,9 +10,9 @@ for file in Database/raw/WikimpactsV2/fullrun/multi_events/table_list/filter/4_1
     echo file name: $filename
     
     poetry run python3 Database/parse_events.py \
-        -r Database/raw/WikimpactsV2/fullrun/multi_events/table_list/filter/4_10\
+        -r Database/raw/WikimpactsV2/fullrun/multi_events/table_list/filter/4_2\
         -f "$filename" \
-        -o Database/output/WikimpactsV2/fullrun/multi_events/table_list_4_10 \
+        -o Database/output/WikimpactsV2/fullrun/multi_events/table_list_4_2 \
         -lvl l1,l3 \
        # echo Parsing $filename has failed!!
 done
