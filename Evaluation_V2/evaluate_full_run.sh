b4af9020a926697eefaab3b074bbbaf576a2aa20
vsc10684 <vsc10684@node301.hydra.os>
2025-07-18 11:34:01 +0200

    update the evaluation results for non en events

--- Evaluation_V2/evaluate_full_run.sh
+++ Evaluation_V2/evaluate_full_run.sh
@@ -65,7 +65,7 @@
 #
 ###################
 
-levels=("l3" )
+levels=("l1" "l3" )
 monetary_impacts=("Damage" "Insured_Damage")
 numerical_impacts=("Deaths" "Injuries" "Affected" "Buildings_Damaged" "Homeless" "Displaced")
 goldFileDir=${1}
